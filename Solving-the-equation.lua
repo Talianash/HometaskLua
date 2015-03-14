22657b009b90e819c62143b4845a1ee987a51c05
Tatiana Shugaeva <t.shugaeva@gmail.com>
2015-03-15 01:12:56 +0300

    v 2.0 of solving-the-equation
    "a" may be equal to 0

--- Solving-the-equation.lua
+++ Solving-the-equation.lua
@@ -8,21 +8,26 @@ print("Enter the free term")
 c = io.read()
 assert(tonumber(c), "You should enter the number")
 -------
-function solvingeq(a, b, c)
+function solvingeq(a , b, c)
 	function Disc(a, b, c)
 		D = b^2 - 4*a*c
 		return D
 	end
 	D = Disc(a, b, c)
-	if  D == 0 then
-		x = (-1)*b/(2*a)
+	if a == "0" then
+		x  = (-c)/b
 		return (x)
-	elseif D < 0 then
-		return (nil)
-	elseif D > 0 then
-		y=(-b + D^(1/2))/(2*a)
-		z=(-b - D^(1/2))/(2*a)
-		return y, z
+	else
+	    if	D == 0 then
+		  x = (-1)*b/(2*a)
+		  return (x)
+	    elseif D < 0 then
+		  return (nil)
+	    elseif D > 0 then
+		   y=(-b + D^(1/2))/(2*a)
+		   z=(-b - D^(1/2))/(2*a)
+		   return y, z
+     	end
 	end
 end
 -------
